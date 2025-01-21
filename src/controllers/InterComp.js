debae4cb425b600177454daa8e33ece8aa128bbc
mops1112 <yotsathonman@hotmail.com>
2025-01-21 12:49:31 +0700

    first commit

--- src/controllers/InterComp.js
+++ src/controllers/InterComp.js
@@ -24,7 +24,7 @@ const dayjs = require("dayjs");
 const ocrService = require("../utils/ocrService");
 const pool = require("../config/db");
 const path = require("path");
-const { insertVehicleWithDetails } = require("../services/vehiclesService");
+const { insertVehicleWithDetails,updateOverview,updatePlates } = require("../services/vehiclesService");
 const baseImagePath = path.join(process.cwd(), "public/snapshots");
 const baseLedPath = path.join(process.cwd(), "public/leds");
 
@@ -61,6 +61,90 @@ class InterComp extends WSController {
     );
   }
 
+  async findAndProcessSnapshots(mappedData) {
+    const [lprSnapshots, overviewSnapshots] = await Promise.all([
+      this.lprSnapshotManager.findSnapshots(mappedData, "lpr"),
+      this.overviewSnapshotManager.findSnapshots(mappedData, "overview"),
+    ]);
+
+    if (lprSnapshots || overviewSnapshots) {
+      // Concurrently send LPR snapshots to OCR, upload LPR image, and upload the overview image
+      const [ocrResult, overviewUploadResult, lprUploadResult] =
+        await Promise.all([
+          lprSnapshots
+            ? ocrService.sendToOCR(lprSnapshots, this.config.ocr_url)
+            : Promise.resolve(null), // No OCR if no LPR snapshots
+          overviewSnapshots
+            ? this.overviewSnapshotManager.uploadImage(
+                overviewSnapshots.imageUrl,
+                "overview"
+              )
+            : Promise.resolve({ success: false }), // No upload if no overview snapshots
+          lprSnapshots
+            ? this.lprSnapshotManager.uploadImage(lprSnapshots.imageUrl, "lpr")
+            : Promise.resolve({ success: false }), // No upload if no LPR snapshots
+        ]);
+
+      // Process OCR results and perform crop uploads if OCR is not null
+      if (ocrResult) {
+        if ([1, 2].includes(mappedData.vehicleClassID)) {
+          // Check if the vehicle is a bus
+          if (isBusByLicensePlate(ocrResult.license_plate)) {
+            return; // Exit early if it's a bus
+          }
+          if (hasNonNumericCharacters(ocrResult.license_plate)) {
+            return;
+          }
+        }
+
+        const cropUploadResult = ocrResult.crop_path
+          ? await this.cropSnapshotManager.uploadImage(
+              ocrResult.crop_path,
+              "crop"
+            )
+          : { success: false };
+
+        // Update OCR data with uploaded file paths
+        if (lprUploadResult.success) {
+          ocrResult.plate_path = lprUploadResult.data.fileUrl;
+        }
+        if (cropUploadResult.success) {
+          ocrResult.crop_path = cropUploadResult.data.fileUrl;
+        }
+
+        // Update mappedData with OCR results
+        mappedData.platePath = ocrResult.plate_path;
+        mappedData.licensePlate = formatLicensePlate(ocrResult.license_plate);
+        mappedData.cropPath = ocrResult.crop_path;
+        mappedData.province = ocrResult.province;
+      } else {
+        // Handle case where OCR result is null
+        if ([1, 2].includes(mappedData.vehicleClassID)) {
+          if (
+            isBusByWheelbase(
+              mappedData.axles[1].wheelbase,
+              this.config.wheelbase_bus
+            )
+          ) {
+            return;
+          }
+        }
+        if (lprUploadResult.success) {
+          mappedData.platePath = lprUploadResult.data.fileUrl;
+        }
+        console.warn("OCR result is null. LPR snapshot uploaded.");
+      }
+
+      // Update overview path if the upload was successful
+      if (overviewUploadResult.success) {
+        mappedData.overviewPath = overviewUploadResult.data.fileUrl;
+      }
+    } else {
+      console.warn("No LPR or Overview snapshots found.");
+    }
+
+    return [lprSnapshots, overviewSnapshots];
+  }
   async handleDataMessage(message) {
     try {
       const rawData = JSON.parse(message);
@@ -75,89 +159,8 @@ class InterComp extends WSController {
       mappedData = mapErrorFlag(mappedData);
 
       // Use Promise.all for concurrent snapshot fetching
-      const [lprSnapshots, overviewSnapshots] = await Promise.all([
-        this.lprSnapshotManager.findSnapshots(mappedData, "lpr"),
-        this.overviewSnapshotManager.findSnapshots(mappedData, "overview"),
-      ]);
-
-      if (lprSnapshots || overviewSnapshots) {
-        // Concurrently send LPR snapshots to OCR, upload LPR image, and upload the overview image
-        const [ocrResult, overviewUploadResult, lprUploadResult] =
-          await Promise.all([
-            lprSnapshots
-              ? ocrService.sendToOCR(lprSnapshots, this.config.ocr_url)
-              : Promise.resolve(null), // No OCR if no LPR snapshots
-            overviewSnapshots
-              ? this.overviewSnapshotManager.uploadImage(
-                  overviewSnapshots.imageUrl,
-                  "overview"
-                )
-              : Promise.resolve({ success: false }), // No upload if no overview snapshots
-            lprSnapshots
-              ? this.lprSnapshotManager.uploadImage(
-                  lprSnapshots.imageUrl,
-                  "lpr"
-                )
-              : Promise.resolve({ success: false }), // No upload if no LPR snapshots
-          ]);
-
-        // Process OCR results and perform crop uploads if OCR is not null
-        if (ocrResult) {
-          if ([1, 2].includes(mappedData.vehicleClassID)) {
-            // Check if the vehicle is a bus
-            if (isBusByLicensePlate(ocrResult.license_plate)) {
-              return; // Exit early if it's a bus
-            }
-            if (hasNonNumericCharacters(ocrResult.license_plate)) {
-              return;
-            }
-          }
-
-          const cropUploadResult = ocrResult.crop_path
-            ? await this.cropSnapshotManager.uploadImage(
-                ocrResult.crop_path,
-                "crop"
-              )
-            : { success: false };
-
-          // Update OCR data with uploaded file paths
-          if (lprUploadResult.success) {
-            ocrResult.plate_path = lprUploadResult.data.fileUrl;
-          }
-          if (cropUploadResult.success) {
-            ocrResult.crop_path = cropUploadResult.data.fileUrl;
-          }
-
-          // Update mappedData with OCR results
-          mappedData.platePath = ocrResult.plate_path;
-          mappedData.licensePlate = formatLicensePlate(ocrResult.license_plate);
-          mappedData.cropPath = ocrResult.crop_path;
-          mappedData.province = ocrResult.province;
-        } else {
-          // Handle case where OCR result is null
-          if ([1, 2].includes(mappedData.vehicleClassID)) {
-            if (
-              isBusByWheelbase(
-                mappedData.axles[1].wheelbase,
-                this.config.wheelbase_bus
-              )
-            ) {
-              return;
-            }
-          }
-          if (lprUploadResult.success) {
-            mappedData.platePath = lprUploadResult.data.fileUrl;
-          }
-          console.warn("OCR result is null. LPR snapshot uploaded.");
-        }
-
-        // Update overview path if the upload was successful
-        if (overviewUploadResult.success) {
-          mappedData.overviewPath = overviewUploadResult.data.fileUrl;
-        }
-      } else {
-        console.warn("No LPR or Overview snapshots found.");
-      }
+      const [lprSnapshots, overviewSnapshots] =
+        await this.findAndProcessSnapshots(mappedData);
       // Create and send LED display image
       // Determine condition image based on `is_overweight`
       const conditionImage = mappedData.is_overweight
@@ -181,6 +184,25 @@ class InterComp extends WSController {
 
       // Send data to WebSocket server
       sendToWebSocket({ vehicleID: vehicleID });
+      if (!mappedData.overviewPath && !mappedData.platePath) {
+        console.warn("Retrying to find snapshots after 5 seconds...",vehicleID);
+        await new Promise((resolve) => setTimeout(resolve, 5000));
+        const [lprSnapshots, overviewSnapshots] =
+          await this.findAndProcessSnapshots(mappedData);
+        if (overviewSnapshots) {
+          await updateOverview(vehicleID, mappedData.overviewPath);
+        }
+        if (lprSnapshots) {
+          await updatePlates(
+            vehicleID,
+            mappedData.licensePlate,
+            mappedData.platePath,
+            mappedData.province,
+            mappedData.cropPath
+          );
+        }
+        sendToWebSocket({ vehicleID: vehicleID });
+      }
     } catch (err) {
       console.error("DataLogger error handling data message:", err);
     }
